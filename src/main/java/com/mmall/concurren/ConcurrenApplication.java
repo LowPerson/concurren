414aa207cb5041e90b8399c315566705f78e1c77
yalishanda <978128416@qq.com>
2019-09-10 15:43:23 +0800

    初始化springboot项目

---
+++ src/main/java/com/mmall/concurren/ConcurrenApplication.java
@@ -0,0 +1,13 @@
+package com.mmall.concurren;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrenApplication.class, args);
+	}
+
+}
